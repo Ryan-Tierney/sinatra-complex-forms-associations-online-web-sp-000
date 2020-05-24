eb0cd7aba05df11e71a28468927143a5785e7c44
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-24 16:12:50 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,15 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    @pets = Pet.all
     erb :'/pets/index' 
-    erb :'/pets/index'
   end
-  end
-
 
   get '/pets/new' do 
-  get '/pets/new' do
     @owners = Owner.all
     erb :'/pets/new'
-    erb :'/pets/new'
   end
-  end
-
 
   post '/pets' do 
-  post '/pets' do
-
     @pet = Pet.create(params[:pet])
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
@@ -29,12 +19,9 @@ class PetsController < ApplicationController
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
-    redirect to "pets/#{@pet.id}"
-  end
   end
 
 
-  get '/pets/:id' do 
   get '/pets/:id/edit' do
     @pet = Pet.find_by_id(params[:id])
     @owners = Owner.all
@@ -43,9 +30,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    @pet = Pet.find(params[:id])
     erb :'/pets/show'
-
   end
 
   post '/pets/:id' do 
